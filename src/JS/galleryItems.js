a2c8c1eccfef2b8a1626d86e24d6e10f4e82368e
Yev1999 <92596319+Yev1999@users.noreply.github.com>
2023-01-29 13:01:01 +0200

    gallary

--- src/JS/galleryItems.js
+++ src/JS/galleryItems.js
@@ -1,38 +1,38 @@
 export const galleryItems = [
   {
-    picture: "/gallery/bedroom-1.jpg",
+    picture: "./gallery/bedroom-1.jpg",
   },
   {
-    picture: "/gallery/bedroom-2.jpg",
+    picture: "./gallery/bedroom-2.jpg",
   },
   {
-    picture: "/gallery/bedroom-3.jpg",
+    picture: "./gallery/bedroom-3.jpg",
   },
   {
-    picture: "/gallery/bedroom-4.jpg",
+    picture: "./gallery/bedroom-4.jpg",
   },
   {
-    picture: "/gallery/bedroom-5.jpg",
+    picture: "./gallery/bedroom-5.jpg",
   },
   {
-    picture: "/gallery/bedroom-6.jpg",
+    picture: "./gallery/bedroom-6.jpg",
   },
   {
-    picture: "/gallery/bedroom-7.jpg",
+    picture: "./gallery/bedroom-7.jpg",
   },
   {
-    picture: "/gallery/bedroom-8.jpg",
+    picture: "./gallery/bedroom-8.jpg",
   },
   {
-    picture: "/gallery/bedroom-9.jpg",
+    picture: "./gallery/bedroom-9.jpg",
   },
   {
-    picture: "/gallery/bedroom-10.jpg",
+    picture: "./gallery/bedroom-10.jpg",
   },
   {
-    picture: "/gallery/bedroom-11.jpg",
+    picture: "./gallery/bedroom-11.jpg",
   },
   {
-    picture: "/gallery/bedroom-12.jpg",
+    picture: "./gallery/bedroom-12.jpg",
   },
 ];
