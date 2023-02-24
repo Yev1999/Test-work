644d7fe07f062b3cfa265e77c10dc09ed1b3e2a0
Yev1999 <92596319+Yev1999@users.noreply.github.com>
2023-02-24 11:29:26 +0200

    add persone carusell

--- src/JS/persone.js
+++ src/JS/persone.js
@@ -1,7 +1,38 @@
-const slide1 = document.querySelector(".splide__list #slide1");
+const teamCompany = [
+  {
+    namePersone: "Jana Dark",
+    position: "Meneger",
+  },
+  {
+    namePersone: "Darya Clept",
+    position: "Seo Meneger",
+  },
+  {
+    namePersone: "JANE GALADRIEL",
+    position: "CEO TENGKUREP",
+  },
+  {
+    namePersone: "Luisa",
+    position: "Cook",
+  },
+  {
+    namePersone: "Jana Dark",
+    position: "Meneger",
+  },
+];
 
-// const generationName = () => {
-//   if (slide1 === ".is-active") {
-//     console.log(111);
-//   }
+const professionName = document.querySelector(".profession-cart");
+
+// const generationName = (teamCompany) => {
+//   console.log(teamCompany);
+//   let team = teamCompany
+//     .map(({ namePersone, position }) => {
+//       return `<h2 class="profession-name">${namePersone}</h2>
+//       <p class="profession-work">${position}</p>`;
+//     })
+//     .join("");
+
+//   professionName.insertAdjacentHTML("afterbegin", team);
 // };
+
+// generationName(teamCompany);
--- src/JS/slide.js
+++ src/JS/slide.js
@@ -2,26 +2,47 @@ import { Splide } from "@splidejs/splide";
 import { URLHash } from "@splidejs/splide-extension-url-hash";
 import "@splidejs/splide/dist/css/themes/splide-skyblue.min.css";
 
-const splide = new Splide(".container-profession", {
-  perPage: 5,
-  perMove: 1,
-  updateOnMove: true,
-  type: "loop",
-  height: "5rem",
-  width: "23rem",
-  focus: "center",
-  padding: 0,
-  gap: "1rem",
-  snap: true,
-  drag: "free",
-});
+// const splide = new Splide(".splideProfession", {
+//   perPage: 5,
+//   perMove: 1,
+//   updateOnMove: true,
+//   type: "loop",
+//   height: "5rem",
+//   width: "23rem",
+//   focus: "center",
+//   padding: 0,
+//   gap: "1rem",
+//   snap: true,
+//   drag: "free",
+// });
 
-splide.mount({ URLHash });
+// splide.mount({ URLHash });
 
-const splideText = new Splide(".splide", {
+const splideText = new Splide("#header-slider", {
   pagination: true,
   perPage: 1,
   arrows: false,
 });
 
 splideText.mount();
+
+const slideTeam = new Splide("#slide-person", {
+  pagination: true,
+  focus: "center",
+  classes: {
+    pagination: "splide__pagination person-class-pagination",
+    page: "splide__pagination__page person-class-page",
+  },
+});
+
+slideTeam.on("pagination:mounted", function (data) {
+  // You can add your class to the UL element
+  data.list.classList.add("splide__pagination--custom");
+
+  // `items` contains all dot items
+  data.items.forEach(function (item) {
+    item.button.classList.add(`person-` + (item.page + 1));
+  });
+});
+
+slideTeam.mount();
