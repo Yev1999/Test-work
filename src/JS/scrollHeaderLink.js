b9f9ebbe49a7d72113c1902447c678b3afe1f1b9
Yev1999 <92596319+Yev1999@users.noreply.github.com>
2023-03-03 19:29:04 +0200

    fix scroll to section

--- src/JS/scrollHeaderLink.js
+++ src/JS/scrollHeaderLink.js
@@ -1,65 +1,12 @@
-const home = document.querySelector(".home");
-const about = document.querySelector(".about");
-const expertise = document.querySelector(".expertise");
-const teams = document.querySelector(".teams");
-const works = document.querySelector(".works");
-const people = document.querySelector(".people");
-const contacts = document.querySelector(".contacts");
-const crollToContacts = document.querySelector(".crollToContacts");
+const buttons = document.querySelectorAll(".header-link button");
 
-const ourStory = document.querySelector(".our-story");
-const play = document.querySelector(".play");
-const competence = document.querySelector(".competence");
-const team = document.querySelector(".team");
-const gallery = document.querySelector(".gallery");
-const profession = document.querySelector(".profession");
-const contact = document.querySelector(".contact");
-
-home.addEventListener("click", (e) => {
-  ourStory.scrollIntoView({
-    block: "nearest",
-    behavior: "smooth",
-  });
-});
-
-about.addEventListener("click", (e) => {
-  play.scrollIntoView({
-    block: "center",
-    behavior: "smooth",
-  });
-});
-
-expertise.addEventListener("click", (e) => {
-  competence.scrollIntoView({
-    block: "center",
-    behavior: "smooth",
-  });
-});
-
-teams.addEventListener("click", (e) => {
-  team.scrollIntoView({
-    block: "center",
-    behavior: "smooth",
-  });
-});
-
-works.addEventListener("click", (e) => {
-  gallery.scrollIntoView({
-    block: "center",
-    behavior: "smooth",
-  });
-});
-
-people.addEventListener("click", (e) => {
-  profession.scrollIntoView({
-    block: "center",
-    behavior: "smooth",
-  });
-});
-
-contacts.addEventListener("click", (e) => {
-  contact.scrollIntoView({
-    block: "center",
-    behavior: "smooth",
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const sectionId = button.dataset.sectionId;
+    const section = document.querySelector(`.${sectionId}`);
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "center",
+    });
   });
 });
