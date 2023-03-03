42f70d040bc610bca67e9b1a197280c99307e0cd
Yev1999 <92596319+Yev1999@users.noreply.github.com>
2023-03-03 17:30:10 +0000

    Deploying to gh-pages from @ Yev1999/Test-work@b9f9ebbe49a7d72113c1902447c678b3afe1f1b9 🚀

---
+++ index.975fb362.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".header-link button").forEach((e=>{e.addEventListener("click",(()=>{const t=e.dataset.sectionId;document.querySelector(`.${t}`).scrollIntoView({behavior:"smooth",block:"center"})}))}));
+//# sourceMappingURL=index.975fb362.js.map
---
+++ index.fc0ef586.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".header-link button").forEach((e=>{e.addEventListener("click",(()=>{const t=e.dataset.sectionId;document.querySelector(`.${t}`).scrollIntoView({behavior:"smooth",block:"center"})}))}));
+//# sourceMappingURL=index.fc0ef586.js.map
